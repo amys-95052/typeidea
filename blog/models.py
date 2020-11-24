e995e1e332de37a4aafd522a5f98ce3034baa042
amys-95052 <18434361278@163.com>
2020-11-24 16:47:59 +0800

    blog APP increase model

--- blog/models.py
+++ blog/models.py
@@ -8,11 +8,11 @@ class Category(models.Model):
     STATUS_NORMAL = 1
     STATUS_DELETE = 0
     STATUS_ITEMS = (
-        ('STATUS_NORMAL','正常'),
-        ('STATUS_DELETE','删除'),
+        (STATUS_NORMAL,'正常'),
+        (STATUS_DELETE,'删除'),
     )
     name = models.CharField(max_length=50,verbose_name='名称')
-    status = models.PositiveIntegerField(default=STATUS_NORMAL,choices=STATUS_ITEMS,verbose_name="状态")
+    status = models.PositiveIntegerField(choices=STATUS_ITEMS,default=STATUS_NORMAL,verbose_name="状态")
     is_nav = models.BooleanField(default=False,verbose_name='是否需要导航')
     owner = models.ForeignKey(User,verbose_name='作者',on_delete=models.CASCADE)
     created_time = models.DateTimeField(auto_now_add=True,verbose_name='创建时间')
@@ -20,16 +20,41 @@ class Category(models.Model):
     class Meta:
         verbose_name = verbose_name_plural = "分类"
 
+    #   区分是否导航
+    @classmethod
+    def get_nave(cls):
+        categories = cls.objects.filter(status=cls.STATUS_NORMAL)
+        '''
+        # 会产生两次数据库查询
+        nav_categories = categories.filter(is_nav = True)
+        normal_categories = categories.filter(is_nav = False)
+        '''
+        nav_categories = []  # 依次数据库查询拿到所有数据，进行遍历
+        normal_categories = []
+        for cate in categories:
+            if cate.is_nav:
+                nav_categories.append(cate)
+            else:
+                normal_categories.append(cate)
+
+        return {
+            'navs': nav_categories,
+            'categories': normal_categories
+        }
+
+    def __str__(self):
+        return self.name
+
 
 class Tag(models.Model):
     STATUS_NORMAL = 1
     STATUS_DELETE = 0
     STATUS_ITEMS = (
-        ('STATUS_NORMAL', '正常'),
-        ('STATUS_DELETE', '删除'),
+        (STATUS_NORMAL, '正常'),
+        (STATUS_DELETE, '删除'),
     )
     name = models.CharField(max_length=50, verbose_name='名称')
-    status = models.PositiveIntegerField(default=STATUS_NORMAL, choices=STATUS_ITEMS, verbose_name="状态")
+    status = models.PositiveIntegerField(choices=STATUS_ITEMS,default=STATUS_NORMAL, verbose_name="状态")
     is_nav = models.BooleanField(default=False, verbose_name='是否需要导航')
     owner = models.ForeignKey(User, verbose_name='作者',on_delete=models.CASCADE)
     created_time = models.DateTimeField(auto_now_add=True, verbose_name='创建时间')
@@ -37,27 +62,63 @@ class Tag(models.Model):
     class Meta:
         verbose_name = verbose_name_plural = "标签"
 
+    def __str__(self):
+        return self.name
+
 
 class Post(models.Model):
     STATUS_NORMAL = 1
     STATUS_DELETE = 0
     STATUS_DRAFT = 2
     STATUS_ITEMS = (
-        ('STATUS_NORMAL', '正常'),
-        ('STATUS_DELETE', '删除'),
-        ('STATUS_DRAFT','草稿')
+        (STATUS_NORMAL, '正常'),
+        (STATUS_DELETE, '删除'),
+        (STATUS_DRAFT,'草稿')
     )
     title = models.CharField(max_length=225,verbose_name="标题")
     desc = models.CharField(max_length=1024,blank=True,verbose_name='摘要')
     content = models.TextField(verbose_name='正文',help_text="正文必须为Markdown格式")
-    status = models.PositiveIntegerField(default=STATUS_NORMAL, choices=STATUS_ITEMS, verbose_name="状态")
+    status = models.PositiveIntegerField(default='STATUS_NORMAL', choices=STATUS_ITEMS, verbose_name="状态")
     category = models.ForeignKey(Category,verbose_name='分类',on_delete=models.CASCADE)    # Category与Post一对多
     tag = models.ManyToManyField(Tag,verbose_name='标签')        # Tag与Post多对多
     owner = models.ForeignKey(User,verbose_name='作者',on_delete=models.CASCADE)   # User与Post一对多
     created_time = models.DateTimeField(auto_now_add=True, verbose_name='创建时间')
+    pv = models.PositiveIntegerField(default=1)      # 统计分一篇的统计量
+    uv = models.PositiveIntegerField(default=1)
 
     class Meta:     # 配置Model的属性
         verbose_name = verbose_name_plural = "文章"
         ordering = ['-id']     # 根据id进行降序排序
 
+    #def __str__(self):
+    #    return self.name
+    # 通过tag和category获取post数据
+    @staticmethod
+    def get_by_tag(tag_id):
+        try:
+            tag = Tag.objects.get(id = tag_id)
+        except Tag.DoesNotExist:
+            tag = None
+            post_list = []
+        else:
+            post_list = tag.post_set.filter(status=Post.STATUS_NORMAL).select_related('owner','category')  # 主动惊醒表连接查询
+        return post_list,tag
 
+    @staticmethod
+    def get_by_category(category_id):
+        try:
+            category = Tag.objects.get(id=category_id)
+        except Tag.DoesNotExist:
+            category = None
+            post_list = []
+        else:
+            post_list = category.post_set.filter(status=Post.STATUS_NORMAL).select_related('owner', 'category')  # 主动表连接查询
+        return post_list, category
+
+    @classmethod
+    def latest_posts(cls):
+        queryset = cls.objects.filter(status = cls.STATUS_NORMAL)
+
+    @classmethod
+    def hot_posts(cls):
+        return cls.objects.filter(status=cls.STATUS_NORMAL).order_by('-pv')
